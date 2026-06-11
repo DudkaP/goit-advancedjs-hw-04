6edea6933854617e301a64a8c0cd85ce82c3d7c1
Dudka Pavlo <dudka.p@outlook.com>
2026-06-11 15:10:24 +0200

    initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '56126477-561bb58c17017209cc05699d2';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  return axios.get(BASE_URL, { params }).then(response => response.data);
+}
